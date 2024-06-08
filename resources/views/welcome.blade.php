38749b617c9d4abd2935289dbf19462eaa3f1e0b
khaledfathi <dev@khaledfathi.com>
2024-06-08 12:30:11 +0300

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>KinsideApps</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <!-- Styles -->
+    <style>
+    </style>
+</head>
+
+<body>
+    <h1>KinsideApps</h1>
+    <hr>
+    <a href="https://play.google.com/store/apps/details?id=com.metro_driver.app&hl=en" target="_blank">
+        <div>
+            <img src="https://play-lh.googleusercontent.com/K3V5vu5eBxhwO77szQ7ZfVkTu4hYsHek1QMhjVaaDCa6mN9hyccq4s2GhMnIeQL2bw=w240-h480"
+                alt="app1_image" width="50">
+            <label for="">مساعد مترو</label>
+        </div>
+    </a>
+</body>
+
+</html>
